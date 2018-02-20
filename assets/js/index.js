fba0249440426023f2432ef6e409da2c5b66350d
camilufer <camilu.fer21@gmail.com>
2018-02-20 16:16:09 -0300

    cambios js

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SEARCH
+var myUrl = 'https://developers.zomato.com/api/v2.1/search';
+var key = 'd79b356002229fc294e09c308a4c8643';
+var cityCode = ['83','73','66','97','257','280'];
+
+$('#option-santiago').click(function(){
+  cityCode.forEach(function(el){
+    $.ajax({
+      url: myUrl,
+      type: 'GET',
+      dataType: 'json',
+      data: {
+      entity_type : 'city',
+        entity_id : el
+      },
+      beforeSend: function(request) {
+        request.setRequestHeader("user-key", key);
+      }
+
+    })
+    .done(function(res){
+      res.restaurants.forEach(function(el){
+        var name = el.restaurant.name;
+        var img = el.restaurant.thumb;
+        var city = el.restaurant.location.city;
+        var cityId = el.restaurant.location.city_id;
+    
+
+    
+      });
+    })
+    
+});
+
+  });
+
