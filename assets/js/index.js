f25d65b3a57cdc73951f88826da475509ff86ece
MrsPepa <jose.leon1996@gmail.com>
2018-02-20 15:56:48 -0300

    Version sign up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,14 @@
-$(document).ready(function({
+`use strict`
+
+var myUrl = 'https://developers.zomato.com/api/v2.1/search';
+var key = 'd79b356002229fc294e09c308a4c8643';
+var cityCode = ['83','73','66','97','257','280'];
+
+
+$(document).ready(function(){
   $('#signUp').hide();
   $('.signUp').click(function(){
     $('#signUp').show();
     $('#welcome').hide();
   })
-}))
+});
